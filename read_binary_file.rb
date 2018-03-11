b9ddfb5c5833602516435c61f867c454373d6d2d
Omar Ali Sheikh <sheikhomar@gmail.com>
2018-03-11 03:32:28 -0700

    Remove old files.

--- read_binary_file.rb
+++
@@ -1,5 +0,0 @@
-f = open('data.bin', 'rb')
-data = bytearray(f.read())
-f.close()
-for i in data:
-  print(i)
--- write_binary_file.rb
+++
@@ -1,4 +0,0 @@
-data = bytearray([1, 2, 3, 4, 5, 6, 7, 8])
-f = open('data.bin', 'wb')
-f.write(data)
-f.close()
