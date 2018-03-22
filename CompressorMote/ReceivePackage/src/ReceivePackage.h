a584af9c722c664093c46f6bcf369ca93f48ae62
kpagels <kelvin@pagels.dk>
2018-03-22 17:04:57 +0100

    store data in flash
    simple program to store in flash

---
+++ CompressorMote/ReceivePackage/src/ReceivePackage.h
@@ -0,0 +1,22 @@
+#ifndef RECEIVE_PACKAGE_H
+#define RECEIVE_PACKAGE_H
+
+ 
+ enum {
+   AM_RECEIVEPACKAGE = 6,
+   TIMER_PERIOD_MILLI = 1000
+ };
+
+typedef nx_struct ReceivePackageMsg {
+  nx_uint8_t type; //if type is 00 then its data otherwise it is end of message.
+  nx_uint8_t datalength;
+  nx_uint8_t data[20];
+} ReceivePackageMsg;
+
+ 
+typedef nx_struct BlinkToRadioMsg {
+  nx_uint16_t nodeid;
+  nx_uint16_t counter;
+} BlinkToRadioMsg;
+
+#endif /* RECEIVE_PACKAGE_H */
